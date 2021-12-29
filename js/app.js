01fd6f631eb80b47be31e8d17d0b2be65acbceed
Mayra Zurita <mizrmx@gmail.com>
2021-12-29 02:39:46 -0600

    last js settings

--- js/app.js
+++ js/app.js
@@ -15,36 +15,68 @@ let data = {}
 fetch('./js/data.json')
     .then(resp => resp.json())
     .then(jsonData => {
+
         jsonData.forEach(element => {
             container.insertAdjacentHTML('beforeend',
-                createRegularCards(element, timeframe));
+                createRegularCard(element, timeframe));
         });
+
+        jsonData.forEach(element => {
+            data[element.title] = element.timeframes;
+        });
+
+        regularCards = document.querySelectorAll('.regular-card');
+        // console.log(regularCards)
     });
 
 // Function
 function menuOnClick(event){
-    console.log('click', event.target.innerText.toLoweCase());
+    // console.log('click', event.target.innerText.toLoweCase());
     menu.forEach(element => {
         element.classList.remove('menu-active');
     });
     event.target.classList.add('menu-active');
-    timeframe = event.target.innerText.toLoweCase();
+    timeframe = event.target.innerText.toLowerCase();
 
     updateCards(timeframe);
 }
 
-function updateCards (timeframe){
-
+function updateCards(timeframe){
+    regularCards.forEach(card => {
+        updateCard(card, timeframe);
+    });
 }
 
-function createRegularCards(element, timeframe) {
+function updateCard(card, timeframe) {
+    const title = card.querySelector('.title').innerText;
+    const current = data[title][timeframe]['current'];
+    const previous = data[title][timeframe]['previous'];
+
+    const timeframeMsg = {
+        'daily': 'Yesterday',
+        'weekly': 'Last Week',
+        'monthly': 'Last Month'
+    };
+
+    const hoursElement = card.querySelector('.hours');
+    hoursElement.innerText = `${current}hrs`;
+    const msgElement = card.querySelector('.description');
+    msgElement.innerText = `${timeframeMsg[timeframe]} - ${previous}hrs`;
+}
+
+function createRegularCard(element, timeframe) {
     let title = element['title'];
     let current = element['timeframes'][timeframe]['current'];
     let previous = element['timeframes'][timeframe]['previous'];
 
-    console.log(title, current);
+    const timeframeMsg = {
+        'daily': 'Yesterday',
+        'weekly': 'Last Week',
+        'monthly': 'Last Month'
+    }
+
     return `
-<div class="regular-card ${title.toLoweCase().replace(/\s/g, '')}"> 
+<div class="regular-card ${title.toLowerCase().replace(/\s/g, '')}"> 
       <div class="property-card">
         <div class="row">
           <div class="title">${title}</div>
@@ -57,7 +89,7 @@ function createRegularCards(element, timeframe) {
 
         <div class="row-2">
           <div class="hours">${current}</div>
-          <div class="description">${previous}</div>
+          <div class="description">${timeframeMsg[timeframe]} - ${previous}</div>
         </div>
       </div>
     </div> `
